ee0afc0f1eb4b35a6515b7f9a37c09ba2b79e687
Richard Kennedy <rkennedy76@bloomberg.net>
2018-11-15 20:28:56 +0000

    Added tests to check ordering of other environment variables in env_check_order.cpp

--- test/env/env_check_order.cpp
+++ test/env/env_check_order.cpp
@@ -21,33 +21,60 @@
 using namespace BloombergLP::recc;
 using namespace std;
 
-TEST(EnvTest, EnvCheckOrder)
+const int MAX = 120;
+
+// parameter is the environment variable, i.e RECC_REMOTE_ENV
+// parameterStr is name of environment variable, i.e "RECC_REMOTE_ENV"
+void permute_test(const vector<const char *> expectedOrderVec,
+                  const map<string, string> parameter,
+                  const string &parameterStr)
 {
-    int counter = 5;
-    const vector<const char *> expectedOrderVec = {
-        "RECC_REMOTE_PLATFORM_arch=x86_64", "RECC_REMOTE_PLATFORM_test=x64_86",
-        "RECC_REMOTE_PLATFORM_zed=win10"};
+    int counter = 0;
+    vector<const char *> permuteVec = expectedOrderVec;
+    do {
+        ++counter;
+        // parse_config_variables expects nullptr at end
+        // sets environ variable, an array of strings terminated by nullptr
+        permuteVec.push_back(nullptr);
 
-    while (counter) {
-
-        vector<const char *> randomVec = expectedOrderVec;
-
-        // shuffle the vector randomly to test permutations.
-        random_shuffle(randomVec.begin(), randomVec.end());
-
-        randomVec.push_back(nullptr);
-
-        parse_config_variables(randomVec.data());
+        parse_config_variables(permuteVec.data());
+        // remove nullptr
+        permuteVec.pop_back();
 
         int v_In = 0;
 
         // ensure ordering is the same as expected Vector
-        for (const auto &platformIter : RECC_REMOTE_PLATFORM) {
-            string map_val = "RECC_REMOTE_PLATFORM_" + platformIter.first +
-                             "=" + platformIter.second;
+        for (const auto &platformIter : parameter) {
+            string map_val =
+                parameterStr + platformIter.first + "=" + platformIter.second;
             EXPECT_EQ(map_val, string(expectedOrderVec[v_In]));
             ++v_In;
         }
-        --counter;
-    }
+    } while (counter < MAX &&
+             next_permutation(permuteVec.begin(), permuteVec.end()));
+}
+
+TEST(EnvTest, PlatformEnvCheckOrder)
+{
+    const vector<const char *> expectedOrderVec = {
+        "RECC_REMOTE_PLATFORM_arch=x86_64", "RECC_REMOTE_PLATFORM_test=x64_86",
+        "RECC_REMOTE_PLATFORM_zed=win10"};
+    permute_test(expectedOrderVec, RECC_REMOTE_PLATFORM,
+                 "RECC_REMOTE_PLATFORM");
+}
+
+TEST(EnvTest, RemoteEnvCheckOrder)
+{
+    const vector<const char *> expectedOrderVec = {
+        "RECC_REMOTE_ENV_PATH=/usr/bin", "RECC_REMOTE_ENV_PWD=/usr",
+        "RECC_REMOTE_ENV_USER=rkennedy76"};
+    permute_test(expectedOrderVec, RECC_REMOTE_ENV, "RECC_REMOTE_ENV");
+}
+
+TEST(EnvTest, DepsEnvCheckOrder)
+{
+    const vector<const char *> expectedOrderVec = {
+        "RECC_DEPS_ENV_OSTYPE=linux-gnu", "RECC_DEPS_ENV_SHELL=/bin/bash",
+        "RECC_DEPS_ENV_USER=rkennedy76"};
+    permute_test(expectedOrderVec, RECC_DEPS_ENV, "RECC_DEPS_ENV");
 }
